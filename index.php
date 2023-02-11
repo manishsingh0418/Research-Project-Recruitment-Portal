ff2f879af834586b530b1d656cdc3af9fcaedaec
Manish Kumar <manishkumar80819@gmail.com>
2023-02-11 20:30:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- divinectorweb.com -->
+<head>
+    <meta charset="UTF-8">
+    <title>VIT Bhopal Research Portal</title>
+    <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="stylemainpage.css">
+</head>
+<body>
+
+<header>
+    <nav>
+    <div class="container">
+            <img src="vitlogo.png" align="left">         
+    &nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp
+    &nbsp&nbsp&nbsp&nbsp&nbsp
+    <button type="button" onclick="window.location.href='index.php'"><h3>Home</h3></button>&nbsp
+    <button><a href="#"><h3>About</h3></a></li></button>&nbsp
+    <button type="button" onclick="window.location.href='#footer'"><h3>Contact Us</h3></button>&nbsp
+    <button><a href="#"><h3>Research Paper</h3></a></li></button>&nbsp
+    <div class="dropdown">
+      <button class="dropbtn"><h3>Login</h3></button>
+      <div class="dropdown-content">
+        <!--<a href="loginadmin.php"   target="_blank">Admin Login</a>-->
+        <a href="loginfaculty.php" target="_blank">Faculty Login</a>
+        <a href="login.php" target="_blank">Student Login</a>
+      </div>
+    </div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <div class="dropdown">
+      <button class="dropbtn"><h3>Signup</h3></button>
+      <div class="dropdown-content">
+        <a href="registration.php"   target="_blank">Student Signup</a>
+        <a href="registrationfaculty.php" target="_blank">Faculty Signup</a>
+      </div>
+    </div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+  </div>
+</nav>
+ </header>   
+    <video width="320" height="240" controls>
+      <source src="movie.mp4" type="video/mp4">
+      <source src="movie.ogg" type="video/ogg">
+      Your browser does not support the video tag.
+    </video>
+    
+    
+    <div class="flip-box">
+      <div class="flip-box-inner">
+        <div class="flip-box-front">
+          <h2>Front Side</h2>
+        </div>
+        <div class="flip-box-back">
+          <h2>Back Side</h2>
+        </div>
+      </div>
+    </div>
+    <div class="scene">
+      <div class="cube">
+        <div class="cube__face cube__face--front">front</div>
+        <div class="cube__face cube__face--back">back</div>
+        <div class="cube__face cube__face--right">right</div>
+        <div class="cube__face cube__face--left">left</div>
+        <div class="cube__face cube__face--top">top</div>
+        <div class="cube__face cube__face--bottom">bottom</div>
+      </div>
+    </div>
+   
+    <!-- FOOTER START -->
+<footer id="footer">
+    <div class="footer">
+      <div class="contain">
+      <div class="col">
+        <h1>More</h1>
+        <ul>
+        <a href="https://vit.ac.in/about-vit">
+          <li>About</li>
+        </a>
+          <a href="https://vit.ac.in/about/vision-mission">
+          <li>Mission</li>
+        </a>
+        </ul>
+      </div>
+      <div class="col">
+        
+      
+        </ul>
+      </div>
+      <div class="col">
+        <h1>University Address</h1>
+        <ul>
+          <li>VIT Bhopal University</li>
+          <li>Bhopal-Indore Highway</li>
+          <li>Kothrikalan, Sehore</li>
+          <li>Madhya Pradesh – 466114</li>
+          <li>Phone: +91/0 75 6025 4500 / 501 / 502</li>
+        </ul>
+      </div>
+      <div class="col">
+        <h1>Quick Links</h1>
+        <ul>
+        <a href="https://vtop.vitbhopal.ac.in/vtop/initialProcess">
+          <li>VTOP</li>
+        </a>
+        <a href="https://vitbhopal.ac.in/women-grievance-cell/">
+          <li>Womens Grievance cell</li>
+        </a>
+        </ul>
+      </div>
+      <div class="col social">
+        <h1>Social</h1>
+        <ul>
+          <a href="https://m.facebook.com/VITUnivBhopal/">
+            <li><img class="sociallogo" src="https://th.bing.com/th/id/OIP.Z4-AYrBledMJM7p04-3vdQAAAA?pid=ImgDet&rs=1" width="25" style="width: 25px;"></li>
+          </a>
+          <a href="https://twitter.com/VITBhopal">
+            <li><img class="sociallogo" src="https://images.thestar.com/hDRGnxdzw6k4hnGfNT87sTLTzl8=/1200x683/smart/filters:cb(2700061000)/https://www.thestar.com/content/dam/thestar/business/2016/06/09/website-says-millions-of-passwords-stolen-from-twitter/twitterlogo.jpg" width="25" style="width: 25px;"></li>
+          </a>
+          <a href="https://www.instagram.com/vit.bhopal">
+            <li><img class="sociallogo" src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg" width="25" style="width: 25px;"></li>
+          </a>
+            <a href="https://youtube.com/@VITBHOPALOfficial">
+              <li><img class="sociallogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/800px-YouTube_full-color_icon_%282017%29.svg.png" width="25" style="width: 25px;"></li>
+          </a>
+        </ul>
+      </div>
+    <div class="clearfix"></div>
+    </div>
+    </div>
+
+</footer>
+
+ <!-- END OF FOOTER -->
+   
+   
+
+
+</body>
+</html>
